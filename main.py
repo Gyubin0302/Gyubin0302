abbb7f5a9d2b04134ed13da2bc52536ed9f66441
coby <67041069+rbsks@users.noreply.github.com>
2023-04-11 09:06:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ markdown_text = """
     <img src="https://img.shields.io/badge/Spring%20Boot-6DB33F?style=flat-sqaure&logo=springBoot&logoColor=white">
     <img src="https://img.shields.io/badge/Spring%20REST%20Doc-6DB33F?style=flat-sqaure&logo=spring&logoColor=white">
     <img src="https://img.shields.io/badge/Spring%20Security-6DB33F?style=flat-sqaure&logo=springSecurity&logoColor=white">
-    <img src="https://img.shields.io/badge/Spring%20Batch-6DB33F?style=flat-sqaure&logo=bookStack&logoColor=white">
+    <!-- <img src="https://img.shields.io/badge/Spring%20Batch-6DB33F?style=flat-sqaure&logo=bookStack&logoColor=white"> -->
     <img src="https://img.shields.io/badge/Spring%20Data%20Jpa-6DB33F?style=flat-sqaure&logo=aqua&logoColor=white">
     <img src="https://img.shields.io/badge/QueryDsl-5d9bb9?style=flat-sqaure&logo=ApacheECharts&logoColor=white">
     <img src="https://img.shields.io/badge/Json%20Web%20Token-442e2e?style=flat-sqaure&logo=jSONWebTokens&logoColor=white">
     <img src="https://img.shields.io/badge/OAuth2-EC1C24?style=flat-sqaure&logo=Authy&logoColor=white">
     <img src="https://img.shields.io/badge/RabbitMQ-FF6600?style=flat-sqaure&logo=rabbitMq&logoColor=white">
-    <img src="https://img.shields.io/badge/Stomp-3b5c6b?style=flat-sqaure&logo=Lospec&logoColor=white">
+    <!-- <img src="https://img.shields.io/badge/Stomp-3b5c6b?style=flat-sqaure&logo=Lospec&logoColor=white"> -->
 </div>
 <h3>DataBase</h3>
 <div>
@@ -46,7 +46,6 @@ markdown_text = """
     <img src="https://img.shields.io/badge/AWS%20EC2-FF9900?style=flat-sqaure&logo=amazonEC2&logoColor=white">
     <img src="https://img.shields.io/badge/AWS%20S3-569A31?style=flat-sqaure&logo=amazonS3&logoColor=white">
     <img src="https://img.shields.io/badge/AWS%20RDS-527FFF?style=flat-sqaure&logo=amazonRDS&logoColor=white">
-    <img src="https://img.shields.io/badge/AWS%20LDS-FF7A00?style=flat-sqaure&logo=hive&logoColor=white">
     <img src="https://img.shields.io/badge/NGINX-009639?style=flat-sqaure&logo=nginx&logoColor=white">
     <img src="https://img.shields.io/badge/Jenkins-D24939?style=flat-sqaure&logo=Jenkins&logoColor=white">
     <img src="https://img.shields.io/badge/Docker-2496ED?style=flat-sqaure&logo=docker&logoColor=white">
